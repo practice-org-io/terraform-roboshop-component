7989e2d1bfe96c48cdf15983433c5afab8dde8b5
lokesh <lokesh.theofficial@gmail.com>
2026-06-28 18:10:10 +0530

    actions

--- locals.tf
+++ locals.tf
@@ -2,8 +2,8 @@ locals {
   ami_id = data.aws_ami.joindevops.id
   private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
   sg_id = data.aws_ssm_parameter.sg_id.value
-  health_check_path = var.component = "frontend" ? "/" : "/health"
-  port_number = var.component = "frontend" ? 80 : 8080
+  health_check_path = var.component == "frontend" ? "/" : "/health"
+  port_number = var.component == "frontend" ? 80 : 8080
   backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
   frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
   alb_listener_arn = var.component == "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
